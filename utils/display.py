a9df1df6fedd7e641fcb7daa0415296c296777f8
davi.oliveira <davi.oliveira@ernestoborges.com.br>
2026-03-30 14:37:58 -0400

    Initial print messages in `utils/display.py`

---
+++ utils/display.py
@@ -0,0 +1,25 @@
+import click
+
+
+def print_error(message: str) -> None:
+    click.echo(click.style(f"  ✖ {message}", fg="red"))
+
+
+def print_banner(version: float) -> None:
+    banner = r"""
+  .___                             ___________
+  |   | _____ _____     ____   ____\_   _____/__________  ____   ____
+  |   |/     \\__  \   / ___\_/ __ \|    __)/  _ \_  __ \/ ___\_/ __ \
+  |   |  Y Y  \/ __ \_/ /_/  >  ___/|     \(  <_> )  | \/ /_/  >  ___/
+  |___|__|_|  (____  /\___  / \___  >___  / \____/|__|  \___  / \___  >
+            \/     \//_____/      \/    \/             /_____/      \/
+  """
+    click.echo(click.style(f"${banner} v.${version}", fg="cyan", bold=True))
+
+
+def print_success() -> None:
+    click.echo(click.style("  ✔ Success", fg="green"))
+
+
+def print_info(message: str) -> None:
+    click.echo(click.style(f"  ℹ Info: ${message}", fg="yellow"))
